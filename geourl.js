ef378c8bcb863c588d4c9a80615a3d9262ecceba
ashishfatnani <40388618+ashishfatnani@users.noreply.github.com>
2020-03-04 17:08:39 +0530

    Create geourl.js

---
+++ geourl.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+const geourl = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(address) +
+    ".json?access_token=pk.eyJ1IjoiYXNoaXNoZmF0bmFuaSIsImEiOiJjazdiazR5OGwwNW1pM2VwdTN5b25oOHc2In0.DlDyG6op2pRG93UK-iGmrg";
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect", undefined);
+    } else if (response.body.features.length == 0) {
+      callback("Unable to find the record Try again", undefined);
+    } else {
+      callback(undefined, {
+        latitude: response.body.features[0].center[1],
+        longitude: response.body.features[0].center[0],
+        location: response.body.features[0].place_name
+      });
+    }
+  });
+};
+
+module.exports = geourl;
